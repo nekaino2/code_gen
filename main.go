dcc67581015e45c1470ec81a4b8567ce40f84807
nekaino2 <mypanda028@gmail.com>
2024-03-25 16:34:29 +0300

    d

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
-package codegen
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type pi struct {
+	k string
+	s int
+	D D
+}
+type D struct {
+	k string
+	l int
+	N N
+}
+type N struct {
+	o string
+	k int
+}
 
 func main() {
-	fmt.Print("f")
+	parseStruct(pi{})
+}
+
+func parseStruct(inter interface{}) {
+	v := reflect.ValueOf(inter)
+	fmt.Println(v)
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if field.Kind() == reflect.Struct {
+			fmt.Println(field)
+			fmt.Printf("%s has a struct type\n", v.Type().Field(i).Name)
+			parseStruct(field.Interface())
+		}
+	}
+	return
 }
